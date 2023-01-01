5c30ea79b4ca7784c2dcf050b94335515b45efb8
startcell <npark@starcell.co.kr>
2023-01-01 12:49:00 +0900

    pipe 수정-모델 파일 복사

--- recipes/KdialectSpeech/Pipeline/run_pipe.py
+++ recipes/KdialectSpeech/Pipeline/run_pipe.py
@@ -49,22 +49,22 @@ if __name__ == "__main__":
         # yaml에서 설정값 읽어오기 : 스토리지 접속 정보, 데이터 저장 위치
         service_name = hparams["service_name"]
         endpoint_url = hparams["endpoint_url"] 
-        storage_region_name = hparams["storage_region_name"]
+        # storage_region_name = hparams["storage_region_name"]
         access_key = hparams["access_key"]
         secret_key = hparams["secret_key"]
 
         s3 = boto3.client(service_name, endpoint_url=endpoint_url, aws_access_key_id=access_key,
                         aws_secret_access_key=secret_key)
 
-        data_save_path = secret_key = hparams["data_save_path"]
-        os.makedirs(data_save_path, exist_ok=True)
-
         bucket_name = hparams["bucket_name"]
-        max_keys = hparams["max_keys"]
         key_names = hparams["key_names"]
+        max_keys = hparams["max_keys"]
 
         data_save_path = hparams["data_save_path"]
         error_file_log = hparams["error_file_log"]
+
+        # data_save_path = secret_key = hparams["data_save_path"]
+        os.makedirs(data_save_path, exist_ok=True)
 
         get_s3_files(s3, bucket_name, key_names, max_keys, data_save_path, error_file_log)
         #####
@@ -82,15 +82,19 @@ if __name__ == "__main__":
         logger.info(f'data_download is not in run_modules')
     # #####
 
-    run_provinces = hparams["run_provinces"]
-    gpu_num = hparams["gpu_num"]
-
-
     #####
     ##### 방언별 실행 : 토크나이저, 언어모델, 음성인식모델
     #####
+
+    run_provinces = hparams["run_provinces"]
+    gpu_num = hparams["gpu_num"]
+    pretrained_model_base = hparams['pretrained_model_base']
+
     for run_province in run_provinces:
         province_option = '--province_code=' + run_province
+        pretrained_model_dir = os.path.join(pretrained_model_base, run_province)
+        logger.info(f'make pretrained_model_dir : {pretrained_model_dir}')
+        os.makedirs(pretrained_model_dir, exist_ok=True)
 
         ##### 토크나이저 실행(방언별로 각각 실행)
         # - 데이터 준비  
@@ -107,6 +111,7 @@ if __name__ == "__main__":
             else:
                 # cmd = ['python', 'train.py', 'hparams/5K_unigram_subword_bpe.yaml', '--device=cpu', province_option] # 리스트로는 실행이 안됨
                 cmd = 'python train.py hparams/5K_unigram_subword_bpe.yaml --device=cpu ' + province_option
+                # cmd = 'python train.py hparams/5K_unigram_subword_bpe.yaml ' + province_option
             
             logger.info(f'cmd : {cmd}')
             result = subprocess.run(cmd,
@@ -117,6 +122,23 @@ if __name__ == "__main__":
                 stdout=subprocess.PIPE,
                 stderr=subprocess.STDOUT
             )
+            logger.info(f'subprocess.CompletedProcess : {subprocess.CompletedProcess}')
+            # if subprocess.CompletedProcess.returncode == 0:
+            if result.returncode == 0:
+                logger.info(f'tokenizer completed successfully')
+                if hparams['copy_trained_model']:
+
+                    token_result_dir = os.path.join(tokenizer_dir, 'results/data_prepared/' + run_province)
+                    if run_province == 'jj': 
+                        tokenizer =  os.path.join(token_result_dir, '1000_unigram.model')
+                    else:
+                        tokenizer =  os.path.join(token_result_dir, '5000_unigram.model')
+
+                    # copy to pretrained_model_dir
+                    tokenizer_target = os.path.join(pretrained_model_dir, 'tokenizer.ckpt')
+                    logger.info(f'tokenizer file copy from {tokenizer} to {tokenizer_target}')
+                    shutil.copy(tokenizer, tokenizer_target)
+
         else:
             logger.info(f'tokenizer is not in run_modules')
 
@@ -142,19 +164,36 @@ if __name__ == "__main__":
                 shell=True,
                 stdout=subprocess.PIPE
             )
+
+            logger.info(f'subprocess.CompletedProcess : {subprocess.CompletedProcess}')
+            # if subprocess.CompletedProcess.returncode == 0:
+            if result.returncode == 0:
+                logger.info(f'LM completed successfully')
+                if hparams['copy_trained_model']:
+
+                    # copy : asr model file for inference
+                    lm_result_dir = os.path.join(lm_dir, 'results/Transformer/5555' + run_province + '/save')
+
+                    lm_model_dir = sorted(glob.glob(lm_result_dir + '/CKPT*'), key=os.path.getmtime)[0]
+                    lm_model_dir = Path(lm_model_dir).stem
+                    lm_model = os.path.join(lm_result_dir, lm_model_dir + 'model.ckpt')
+                    lm_target = os.path.join(pretrained_model_dir, 'lm.ckpt')
+                    logger.info(f'lm file copy from {lm_model} to {lm_target}')
+                    shutil.copy(lm_model, lm_target)
+
         else:
             logger.info(f'lm is not in run_modules')
 
         #####
 
-    # 음성인식 실행(방언별로 각각 실행)  
-    # 추론 준비 : 추론에 필요한 모델 파일을 한 디렉토리에 복사
-
+        # 음성인식 실행(방언별로 각각 실행)  
+        # 추론 준비 : 추론에 필요한 모델 파일을 한 디렉토리에 복사
 
         ##### ASR
         if 'asr' in hparams['run_modules']:
             logger.info(f'asr run_option : {province_option}')
             logger.info(f'gpu num : {gpu_num}')
+            logger.info(f'province : {run_province}')
             asr_dir = hparams["asr_dir"]
             print(asr_dir)
             if gpu_num > 1: # 제주도일 경우 처리(vocab size가 다름)
@@ -182,30 +221,29 @@ if __name__ == "__main__":
                 logger.info(f'asr completed successfully')
                 if hparams['copy_trained_model']:
 
-                    train_result_dir = os.path.join(asr_dir, 'results/Conformer/7774/' + run_province + '/save')
+                    # copy :  hparam file for inference
+                    hparam_file = os.path.join(pretrained_model_base, 'hyperparams.yaml')
+                    hparam_file_target = os.path.join(pretrained_model_dir, 'hyperparams.yaml')
+                    logger.info(f'hparma file copy from {hparam_file} to {hparam_file_target}')
+                    shutil.copy(hparam_file, hparam_file_target)
 
-                    lm_model =  os.path.join(train_result_dir, 'lm.ckpt')
-                    tokenizer =  os.path.join(train_result_dir, 'tokenizer.ckpt')
+                    # copy : asr model file for inference
+                    train_result_dir = os.path.join(asr_dir, 'results/Conformer/5555/' + run_province + '/save')
 
                     best_model_dir = sorted(glob.glob(train_result_dir + '/CKPT*'), key=os.path.getmtime)[0]
                     best_model_dir = Path(best_model_dir).stem
+                    best_model = os.path.join(train_result_dir, best_model_dir, 'model.ckpt')
+                    best_model_target = os.path.join(pretrained_model_dir, 'asr.ckpt')
+                    logger.info(f'asr model file copy from {best_model} to {best_model_target}')
+                    shutil.copy(best_model, best_model_target)
 
-                    best_model = os.path.join(best_model_dir + '/model.ckpt')
-                    normalizer = os.path.join(best_model_dir + '/normalizer.ckpt')
+                    # copy : normalizer file for inference
+                    normalizer = os.path.join(train_result_dir, best_model_dir + '/normalizer.ckpt')
+                    normalizer_target = os.path.join(pretrained_model_dir, 'normalizer.ckpt')
+                    logger.info(f'normalizer file copy from {best_model} to {best_model_target}')
+                    shutil.copy(normalizer, normalizer_target)
 
-                    pretrained_model_base = hparams['pretrained_model_base']
-                    pretrained_model_dir = os.path.join(pretrained_model_base, run_province)
-
-                    hparam_file = os.path.join(pretrained_model_base, 'hyperparams.yaml')
-
-                    # copy to pretrained_model_dir
-                    shutil.copy(hparam_file, pretrained_model_dir)
-                    shutil.copy(lm_model, pretrained_model_dir)
-                    shutil.copy(tokenizer, pretrained_model_dir)
-                    shutil.copy(best_model, pretrained_model_dir)
-                    shutil.copy(normalizer, pretrained_model_dir)
-
-                    os.symlink(best_model, lm_model)
+                    # os.symlink(best_model, lm_model)
 
         else:
             logger.info(f'asr is not in run_modules')
